5582fe0a646bee09c3b9ee35497c62196fbdf110
andrew-wiggin <lyjmxb@gmail.com>
2017-09-21 22:45:58 +0800

    capacity sensor data reading test

--- input.py
+++ input.py
@@ -1,2 +1,18 @@
-def sdf_input(path, diam)
+def Sdf_input(path, diam):
     return path
+def Cap_input(path, sensor_ch):
+    import numpy as np
+    tmp = []
+    with open(path, 'r') as f:
+        for line in f:
+            ws = line.split(' ')
+            tmp.append(ws[5].replace(',', '.'))
+    marr = np.array(tmp, dtype='double')
+    return marr;
+    
+path = 'cap_result_test.txt'
+sensor_ch = 5
+marr = Cap_input(path, sensor_ch)
+print(marr)
+    
+    
